f1d0ce3fe8426ea4db425a31916295d460e2c81d
Adem Bouatay <adem.bouatay@eniso.u-sousse.tn>
2024-05-21 18:10:58 +0100

    added .py .ts .java and .c extensions

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Logs-Remover
+A python script to remove logs from code, it works with js, ts, py, java and c
--- code.py
+++ code.py
@@ -1,21 +1,33 @@
 import sys
+extenstions = {"js", "py", "ts", "java", "c"};
 
 def checkExtension(fileName):
     extension = fileName.split(".");
-    if extension[1] != "js":
-        raise Exception("Files must be .js files!!");
+    if extension[1] not in extenstions:
+        raise Exception(f"Files must be {extenstions} files!!");
     
-def logRemover(fileLines):
+def logRemover(fileLines, langLog):
     newFileContent = [];
     for line in fileLines:
-        if "console.log" in line:
+        if langLog in line:
             content = line.split(";");
-            content = [l for l in content if "console.log" not in l];
+            content = [l for l in content if langLog not in l];
             content = ';'.join(content);
             newFileContent.append(content);
         else:
             newFileContent.append(line);
     return ''.join(newFileContent);
+
+def fileManager(arg, langLog):
+    file = open(arg, 'r');
+    lines = file.readlines();
+    file.close();
+    file = open(arg, 'w');
+    file.write(logRemover(lines, langLog));
+    file.close();
+    file = open(arg, 'r');
+    print(file.read());
+    print("done!!")
 
 def main():
     if len(sys.argv) < 2:
@@ -26,17 +38,23 @@ def main():
     print(f"Arguments: {arguments}");
     for arg in arguments:
         try:
+            extension = arg.split(".")[1];
             checkExtension(arg);
-            file = open(arg, 'r');
-            lines = file.readlines();
-            file.close();
-            file = open(arg, 'w');
-            file.write(logRemover(lines));
-            file.close();            
+            match extension:
+                case "js":
+                    fileManager(arg, "console.log");
+                case "ts":
+                    fileManager(arg, "console.log");
+                case "py":
+                    fileManager(arg, "print");
+                case "java":
+                    fileManager(arg, "System.out.println");
+                case "c":
+                    fileManager(arg, "printf");
         except Exception as e:
             print(e);
-        except IOError as e:
-            print(e);
+        except IOError as err:
+            print(err);
 
 if __name__ == "__main__":
     main();
